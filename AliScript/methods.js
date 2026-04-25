d28d7fd3467b66e30bab4e22ee018380b5afd1ff
ALITAJ ALAM <alitajalam645@gmail.com>
2026-04-25 13:41:22 +0000

    methods basic

---
+++ AliScript/methods.js
@@ -0,0 +1,23 @@
+const f1="Alitaj"
+const fl="alam"
+const fullname=f1+fl
+console.log(fullname);
+console.log(`Hello my firstnname is ${f1} and last name is  ${fl}`);
+const GameName = new String ('Alitaj');
+
+
+console.log(GameName[0]);
+console.log(GameName.__proto__);
+console.log(GameName.length);
+console.log(GameName.toUpperCase());
+
+console.log(GameName.charAt(1));
+
+const newString=GameName.substring(0,3);
+console.log(newString);
+const nameString=GameName.slice(0,);
+console.log(nameString);
+let gmail="alitajalam645@gmail.com";
+console.log(gmail.replace('gmail','outlook'));
+
+
