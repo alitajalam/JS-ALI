e337c6c696fa131b1899ff0ae6ef986cac182d0a
ALITAJ ALAM <alitajalam645@gmail.com>
2026-04-26 03:21:18 +0000

    math and num

---
+++ AliScript/math.js
@@ -0,0 +1,25 @@
+const score = 100;
+console.log(score);
+
+const newScore=new Number(100)
+console.log(newScore);
+console.log(newScore.toString().length);
+console.log(newScore.toFixed(2));
+const newString=new Number(12.82649)
+console.log(newString.toPrecision(2));
+const num=10000000000;
+console.log(num.toLocaleString());
+console.log(num.toLocaleString('en-IN'));
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+console.log(Math)
+console.log(Math.abs(-4))//4
+console.log(Math.round(4.6))//5
+console.log(Math.ceil(4.1))//5
+console.log(Math.floor(4.9)) //4
+console.log(Math.min(8,9,5,2,6,7))//2
+console.log(Math.random())//the value between 0 to 1 
+
+
+
