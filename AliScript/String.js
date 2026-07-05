63b72360fc474ef328b9c59e6042ab59a9d71f82
ALITAJ ALAM <alitajalam645@gmail.com>
2026-07-05 12:17:18 +0000

    stringbasic

---
+++ AliScript/String.js
@@ -0,0 +1,10 @@
+// const name = "Alitaj"
+const age =22
+// console.groupCollapsed(name+age);
+// console.log(`Hello my name is ${name} and my age is ${age}`);
+const name= new String('Alitaj')
+console.log(name.charAt(2));
+console.log(name.indexOf('j'))
+const stdep="Alitaj_Alam";
+const p=stdep.slice(0,5);
+console.log(p);
