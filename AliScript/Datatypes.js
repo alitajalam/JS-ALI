0829bcf3ef9bc27d03cb5bbb376a3d28609e3758
ALITAJ ALAM <alitajalam645@gmail.com>
2026-07-05 09:45:51 +0000

    variables and datatypes

---
+++ AliScript/Datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+// alert(2+3) we are using nodejs , not browser
+console.log("Alitaj")
+let name = "Ali"
+let age=22
+let isLoggedIn=true
+// number== 2 to power 53
+//bigint 
+//string
+// boolean== true/false
+// null==standalone value or empty
+// undefinned== not assigned value
+// symbol== unique
+// object 
+console.log(typeof "Alitaj");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined);// undefined
---
+++ AliScript/variables.js
@@ -0,0 +1,15 @@
+const AccountId=123456
+let accountEmail="Alitaj@google.com"
+var accountPassword="123456789"
+accountCity="NKE"
+let accountState;
+// console.log(AccountId)
+// console.log(accountEmail)
+// console.log(accountPassword)
+// console.log(accountCity)
+// console.log(accountState)
+console.table([AccountId,accountPassword,accountState,accountEmail,accountCity])
+/*
+prefer not use var 
+because of issue in block scope and functional scope
+*/
