41f6f4ea5904fea19808b43cd64ef214c19c9389
ALITAJ ALAM <alitajalam645@gmail.com>
2026-07-05 12:18:10 +0000

    datatypesbasic

--- AliScript/Datatypes.js
+++ AliScript/Datatypes.js
@@ -16,5 +16,5 @@ console.log(typeof "Alitaj");
 console.log(typeof age);
 console.log(typeof null); // object
 console.log(typeof undefined);// undefined
-// Stack memory store premitive value examples String ,Number,Boolean ,null,undefined,symbole 
+// Stack memory store premitive value examples String ,Number,Boolean ,null,undefined,symbole ,BigInt
 // Heap memory store not premitive/reference value examples object, array,function
